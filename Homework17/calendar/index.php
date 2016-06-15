d3a895610dde08442ac5486d0481271d42d6dc2f
Hrant75 <hhro@yahoo.com>
2016-06-15 18:02:06 +0400

    calendar

--- Homework17/calendar/index.php
+++ Homework17/calendar/index.php
@@ -78,57 +78,6 @@
                     </div>
                 </div>
             </div>
-
-
-<!---->
-<!--            -->
-<!--            <div class="row">-->
-<!--            <br>-->
-<!--            <form action="--><?//=$_SERVER["PHP_SELF"]?><!--">-->
-<!--                <div class="form-group">-->
-<!--                    <label for="year">Year</label>-->
-<!--                    <input type="number" value="--><?//=$currentYear?><!--" max="9999" id="year" name="year" class="form-control" required>-->
-<!--                </div>-->
-<!--                <button type="submit" class="btn btn-info form-control">Calculate Friday 13's of this year</button>-->
-<!--            </form>-->
-<!--            <div class="alert alert-info" role="alert">-->
-<!--                --><?php
-//
-//                function friday13($year){
-//                    $count = 0;
-//                    $days = [];
-//
-//                    for($i = 1; $i < 13; $i++){
-//                        $mk = mktime(0,0,0,$i,13,$year);
-//                        if(date("w", $mk) == 5){
-//                            $count++;
-//                            $days[] = $mk;
-//                        }
-//                    }
-//
-//                    if (count($days)>1){
-//                        echo 'There are '.$count.' "Friday 13"s in '.$year.'<br> And they are ';
-//                        for($i=0; $i<count($days); $i++) {
-//                            echo date("d-m-Y", $days[$i]);
-//                            if ($i == count($days) - 1) {
-//                                echo '.';
-//                            } else {
-//                                echo ', ';
-//                            }
-//                        }
-//                    } else {
-//                        echo 'There is only one "Friday 13" in '.$year.'<br>';
-//                        echo 'And this date is '.date("d-m-Y",$days[0]);
-//                    }
-//                }
-//
-//                friday13($currentYear);
-//                ?>
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="col-md-4">-->
-<!--            </div>-->
-<!--        </div>-->
     </body>
 </html>
 
